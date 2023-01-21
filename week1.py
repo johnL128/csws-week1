2a8145c72cbf92bdd69023918aef578949b96f70
John-Louie <john_louie@icloud.com>
2023-01-21 13:53:10 +0000

    added arrays/list, simple arithmetic into program

--- week1.py
+++ week1.py
@@ -1,2 +1,18 @@
+#Simple Hello World Program with loop
 for i in range (10):
     print ("Hello World " + str(i))
+    
+#Simple Arithmetic
+for x in range (5):
+    y = 5 + x
+    print (y)
+    
+#Simple List (Array)
+cars = ["BMW", "audi", "ferrari", "porsche"]
+print (cars)
+
+#.title() capitalises first letter of the str
+print (cars[3].title())
+
+#Prints last item on the list
+print (cars[-1])
